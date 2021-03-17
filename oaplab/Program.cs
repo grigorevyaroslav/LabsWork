0bb223ca76aa5934991be4a17a1f9cb4744679ec
demo <demo@yotc.ru>
2021-03-17 10:53:05 +0300

    общие сведения о под программах

--- oaplab/Program.cs
+++ oaplab/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 // namespace такой же как и название проекта
-namespace oapLabaBati
+namespace oapLabaGrigorev
 {
     // про классы мы пока не говорили...
     class Program
